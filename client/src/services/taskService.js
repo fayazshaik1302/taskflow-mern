5ebb3cc757b38815795b91e6dda26c49292a6216
Shaik Fayaz <fayazshaik1302@gmail.com>
2026-07-01 15:53:22 +0530

    Update API URL for production

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: "https://taskflow-server-uk5x.onrender.com/api/tasks",
 });
 
 export const getTasks = () => API.get("/");
